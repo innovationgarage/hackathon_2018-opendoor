35e090c29a9ddaaf29ab4c991a0a1b37c2570586
Erwin Ried <erwinried@gmail.com>
2018-06-28 13:46:24 +0200

    Firmware

---
+++ firmware/EnterDualog/EnterDualog.ino
@@ -0,0 +1,77 @@
+#include <time.h>
+#include <BlynkSimpleEsp8266.h>
+#include <ESP8266WiFi.h>
+#include <Servo.h>
+#include "credentials.h"
+#include "config.h"
+
+#define BLYNK_PRINT Serial
+
+const int timezone = TIME_TIMEZONE, dst = TIME_DST;
+Servo door;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Connecting to WIFI ...");
+  Blynk.begin(BLYNK_HASH, WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED);
+  Serial.println("Connected.");
+
+  configTime(TIME_TIMEZONE * 3600, TIME_DST * 0, "pool.ntp.org", "time.nist.gov");
+  Serial.println("\nWaiting for time ...");
+  while (!time(nullptr));
+  Serial.println("Time received.");
+
+  door.attach(DOOR_PIN);
+  door.write(DOOR_ANGLE_CLOSE);
+}
+
+void openDoor()
+{
+  Serial.println("Opening door ...");
+  door.write(DOOR_ANGLE_OPEN);
+  delay(DOOR_DELAY);
+  door.write(DOOR_ANGLE_CLOSE);
+}
+
+void printTime()
+{
+  time_t now;
+  struct tm * timeinfo;
+  time(&now);
+  timeinfo = localtime(&now);
+  Serial.print(timeinfo->tm_hour);
+  Serial.print(":");
+  Serial.print(timeinfo->tm_min);
+  Serial.print(" DOW:");
+  Serial.print(timeinfo->tm_wday);
+}
+
+bool isDoorEnabled()
+{
+  time_t now;
+  struct tm * timeinfo;
+  time(&now);
+  timeinfo = localtime(&now);
+
+  return (timeinfo->tm_hour >= TIME_HOUR_OPEN && timeinfo->tm_hour < TIME_HOUR_CLOSE && timeinfo->tm_wday > 1 && timeinfo->tm_wday < 7);
+}
+
+BLYNK_WRITE(V0)
+{
+  if (param.asInt())
+    if (isDoorEnabled()) openDoor();
+    else
+    {
+      Serial.println("Not opening the door.");
+      printTime();
+    }
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
---
+++ firmware/EnterDualog/config.h
@@ -0,0 +1,10 @@
+#define DOOR_PIN D4
+#define DOOR_DELAY 300
+#define DOOR_ANGLE_OPEN 115
+#define DOOR_ANGLE_CLOSE 80
+
+#define TIME_HOUR_OPEN 8 // Inclusive
+#define TIME_HOUR_CLOSE 19 // Not inclusive
+
+#define TIME_DST 0
+#define TIME_TIMEZONE 2
