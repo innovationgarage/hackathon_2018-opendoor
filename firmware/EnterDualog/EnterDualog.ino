b600bb1ee471c3fcb5bacf04dd9b870eee5bfd77
Erwin Ried <erwinried@gmail.com>
2018-07-02 15:04:50 +0200

    New firmware with hodor functionality

--- firmware/EnterDualog/EnterDualog.ino
+++ firmware/EnterDualog/EnterDualog.ino
@@ -7,8 +7,14 @@
 
 #define BLYNK_PRINT Serial
 
+bool hodor = false;
 const int timezone = TIME_TIMEZONE, dst = TIME_DST;
 Servo door;
+unsigned long nextHodor = 0;
+int hodorRemaining = 0;
+
+// Attach virtual serial terminal to Virtual Pin V1
+WidgetTerminal terminal(V1);
 
 void setup()
 {
@@ -23,10 +29,16 @@ void setup()
   Serial.println("\nWaiting for time ...");
   while (!time(nullptr));
   Serial.println("Time received.");
+
+  terminal.println("DOOR OPENER v0.1 started");
+  terminal.flush();
 }
 
 void openDoor()
 {
+  terminal.println("Opening the door");
+  terminal.flush();
+
   Serial.println("Opening door ...");
   door.attach(DOOR_PIN);
   door.write(DOOR_ANGLE_OPEN);
@@ -55,22 +67,71 @@ bool isDoorEnabled()
   time(&now);
   timeinfo = localtime(&now);
 
-  return (timeinfo->tm_hour >= TIME_HOUR_OPEN && timeinfo->tm_hour < TIME_HOUR_CLOSE && timeinfo->tm_wday > 1 && timeinfo->tm_wday < 7);
+  return (timeinfo->tm_hour >= TIME_HOUR_OPEN && timeinfo->tm_hour < TIME_HOUR_CLOSE && timeinfo->tm_wday >= 1 && timeinfo->tm_wday < 7);
+}
+
+void tryToOpenDoor()
+{
+  if (isDoorEnabled()) openDoor();
+  else
+  {
+    hodor = false;
+    terminal.println("Use the normal key to open the door");
+    terminal.flush();
+
+    Serial.println("Not opening the door.");
+    printTime();
+  }
 }
 
 BLYNK_WRITE(V0)
 {
   if (param.asInt())
-    if (isDoorEnabled()) openDoor();
-    else
-    {
-      Serial.println("Not opening the door.");
-      printTime();
-    }
+    tryToOpenDoor();
+}
+
+BLYNK_WRITE(V1)
+{
+  String cmd = param.asStr();
+  if (String(HODOR_ENABLE) == cmd) {
+    terminal.println("Walder is holding the door");
+    hodorRemaining = HODOR_TIMES;
+    hodor = true;
+  }
+  else if (String(HODOR_DISABLE) == cmd) {
+    terminal.println("Hodor is defeated");
+    hodor = false;
+  } else {
+    terminal.println("Command not supported");
+  }
+
+  terminal.flush();
+}
+
+BLYNK_WRITE(V2)
+{
+  int s = param.asInt();
+  terminal.print(s ? "GPS: Enter" : "GPS: Exit");
+  terminal.flush();
 }
 
 void loop()
 {
   Blynk.run();
+
+  if (hodor && millis() > nextHodor)
+  {
+    if (hodorRemaining-- > 0)
+    {
+      nextHodor = millis() + HODOR_REPEAT;
+      tryToOpenDoor();
+    }
+    else
+    {
+      hodor = false;
+      terminal.println("Walder will rest now");
+      terminal.flush();
+    }
+  }
 }
 
--- firmware/EnterDualog/config.h
+++ firmware/EnterDualog/config.h
@@ -3,7 +3,12 @@
 #define DOOR_ANGLE_OPEN 115
 #define DOOR_ANGLE_CLOSE 80
 
-#define TIME_HOUR_OPEN 8 // Inclusive
+#define HODOR_REPEAT 5000
+#define HODOR_TIMES 250
+#define HODOR_ENABLE "hodor"
+#define HODOR_DISABLE "whites"
+
+#define TIME_HOUR_OPEN 7 // Inclusive
 #define TIME_HOUR_CLOSE 19 // Not inclusive
 
 #define TIME_DST 0
